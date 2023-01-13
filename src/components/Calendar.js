818f7775f8b9326f02c852419a0a345fbc22e0fe
Justin <justin.thareja@gmail.com>
2023-01-12 19:43:21 -0800

    add Calendar

---
+++ src/components/Calendar.js
@@ -0,0 +1,12 @@
+export function Calendar() {
+  return (
+    <iframe
+      title="calendar"
+      src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&showNav=1&title=Yoga%20Classes%20and%20Events%20in%20Plumas%20County&src=NWIxMjQ4NmY4MTM4NjEyN2I5ZGExZGMxODMwYTc1OGExYzY1OTBkMTEyYjZkNGUxYjk3YWNhNmQwNGM5NmQwZkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23E67C73"
+      style={{ borderWidth: 0 }}
+      width="800"
+      height="600"
+      frameborder="0"
+    ></iframe>
+  );
+}
