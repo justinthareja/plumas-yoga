b88d20af9da1364cee5a9e36f76613c24e48f8f8
Justin <justin.thareja@gmail.com>
2023-01-23 14:39:39 -0800

    add link to public calendar url

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ function App() {
   return (
     <div className="calendar">
       <Calendar />
+      {/* TODO: Find a more elegant solution for this:
+      https://support.google.com/calendar/thread/167345067?hl=en */}
+      <p>
+        Can't see the calendar?{" "}
+        <a href="https://calendar.google.com/calendar/embed?src=5b12486f81386127b9da1dc1830a758a1c6590d112b6d4e1b97aca6d04c96d0f%40group.calendar.google.com&ctz=America%2FLos_Angeles">
+          Click here
+        </a>
+      </p>
     </div>
   );
 }
