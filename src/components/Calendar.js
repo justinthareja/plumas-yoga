a1d837f987ec427b7acd02e1b2f078b9433c2e07
Justin <justin.thareja@gmail.com>
2023-01-27 13:26:07 -0800

    update fullcalendar options

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -4,20 +4,26 @@ import { Calendar as FullCalendar } from "@fullcalendar/core";
 import googleCalendarPlugin from "@fullcalendar/google-calendar";
 import interactionPlugin from "@fullcalendar/interaction";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import listPlugin from "@fullcalendar/list";
 
 const googleCalendarApiKey = "AIzaSyCzWol4EXLrlxgi1vUrPzOsTkUUiM17Bg4";
 const googleCalendarId =
-  "5b12486f81386n127b9da1dc1830a758a1c6590d112b6d4e1b97aca6d04c96d0f@group.calendar.google.com";
-const googleOAuthClientId =
-  "676890687623-8s3fvm9pg5c67om74rfvekqefaeppasu.apps.googleusercontent.com";
+  "5b12486f81386127b9da1dc1830a758a1c6590d112b6d4e1b97aca6d04c96d0f@group.calendar.google.com";
 
-const plugins = [googleCalendarPlugin, interactionPlugin, dayGridPlugin];
+const plugins = [
+  googleCalendarPlugin,
+  interactionPlugin,
+  dayGridPlugin,
+  timeGridPlugin,
+  listPlugin,
+];
 
 export function Calendar() {
   const calendarRef = React.useRef(null);
+  const [googleCalendar, setGoogleCalendar] = React.useState(null);
 
   // Initalize FullCalendar
-  const [googleCalendar, setGoogleCalendar] = React.useState(null);
   React.useEffect(() => {
     if (calendarRef.current === null || googleCalendar !== null) {
       return;
@@ -26,10 +32,19 @@ export function Calendar() {
     const calendar = new FullCalendar(calendarRef.current, {
       plugins,
       googleCalendarApiKey,
-      initialView: "dayGridMonth",
       events: {
         googleCalendarId,
         className: "gcal-event", // an option!
+      },
+      eventClick: handleEventClick,
+      initialView: "dayGridMonth",
+      headerToolbar: {
+        left: "today prev,next",
+        center: "title",
+        right: "timeGridWeek,dayGridMonth,listWeek",
+      },
+      buttonText: {
+        list: "agenda",
       },
     });
 
@@ -38,6 +53,13 @@ export function Calendar() {
     setGoogleCalendar(calendar);
   }, [calendarRef, googleCalendar]);
 
-  console.log("googleCalendar", googleCalendar);
+  function handleEventClick(info) {
+    info.jsEvent.preventDefault(); // don't let the browser navigate
+
+    if (info.event.url) {
+      window.open(info.event.url);
+    }
+  }
+
   return <div ref={calendarRef}></div>;
 }
